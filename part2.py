9a6fc5f6f20d35208c7a29f2c941528b6cbdedfd
mnkono <71945757+mnkono@users.noreply.github.com>
2026-03-23 21:59:00 -0700

    part 2 creation
    part 2 copied from canvas

---
+++ part2.py
@@ -0,0 +1,18 @@
+# Group #: B42
+# Student names: Mfon Nkono and Saranga Varma
+ 
+import threading
+import queue
+import time, random
+
+def consumerWorker (queue):
+    """target worker for a consumer thread"""
+    # to implement  
+  
+def producerWorker(queue):
+    """target worker for a producer thread"""
+    # to implement
+
+if __name__ == "__main__":
+    buffer = queue.Queue()
+    # to implement
